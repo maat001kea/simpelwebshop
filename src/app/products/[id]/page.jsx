807dca9817089f22074cd1b5611088cf903130e2
Viktor <viho0002@stud.kea.dk>
2025-04-23 09:14:48 +0200

    subgridfixdone

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <div>id</div>;
+  return (
+    <main>
+      <div>id</div>
+    </main>
+  );
 }
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <div>products</div>;
+  return (
+    <main>
+      <div>products</div>
+    </main>
+  );
 }
