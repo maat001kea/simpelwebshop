8dc84ae5052d9770436e9cff514d017a00e0c0c8
Mahmoud Atie <maat0001@stud.kea.dk>
2025-04-24 12:29:10 +0200

    nyt

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -5,15 +5,15 @@ import { IoCartOutline } from "react-icons/io5";
 const Header = () => {
   return (
     <header className="sticky top-0 z-50 bg-white grid grid-cols-subgrid" style={{ gridColumn: "full-bleed" }}>
-      <div className="flex flex-wrap sm:flex-nowrap justify-between items-center py-4 px-4 sm:px-0" style={{ gridColumn: "content" }}>
+      <div className="flex flex-wrap sm:flex-wrap justify-between items-center py-4 px-4 sm:px-0" style={{ gridColumn: "content" }}>
         {/* Left: Logo + Product */}
         <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-8 w-full sm:w-auto">
           {/* Logo */}
-          <Link href="/" className="flex items-center text-orange-500 text-4xl sm:text-4xl font-bold whitespace-nowrap">
+          <Link href="/" className="flex items-center text-orange-500 text-5xl sm:text-6xl font-bold whitespace-nowrap">
             <span>B</span>
-            <span className="text-lg sm:text-x2 font-semibold ml-1">uzz</span>
+            <span className="text-2xl sm:text-3xl font-semibold ml-0">uzz</span>
             <span>B</span>
-            <span className="text-lg sm:text-x2 font-semibold ml-1">asket</span>
+            <span className="text-2xl sm:text-3xl font-semibold ml-0">asket</span>
           </Link>
 
           {/* Product link */}
@@ -23,7 +23,8 @@ const Header = () => {
         </div>
 
         {/* Right: Cart */}
-        <div className="mt-2 sm:mt-0 text-orange-500  sm:text-xl">
+
+        <div className="mt-2 sm:mt-0 text-orange-500 text-4xl sm:text-5xl">
           <Link href="/payment">
             <IoCartOutline className="hover:scale-110 transition-transform duration-200 cursor-pointer" />
           </Link>
