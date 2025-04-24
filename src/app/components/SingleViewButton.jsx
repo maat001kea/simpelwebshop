af060999a1618bb393cc884ad526f140668a7ca1
Farwa Rizvi <farwamehdi3@gmail.com>
2025-04-24 10:02:11 +0200

    check

--- src/app/components/SingleViewButton.jsx
+++ src/app/components/SingleViewButton.jsx
@@ -2,7 +2,7 @@
 const SingleButton = () => {
   return (
     <div>
-      <button className="bg-[#F27F3D] rounded-lg text-white font-bold py-2 px-4  hover:bg-orange-600 transition duration-300 ease-in-out">View Product</button>
+      <button className="bg-[#F27F3D] rounded-lg text-white font-bold py-2 px-4  hover:bg-orange-600 transition duration-300 ease-in-out">hello</button>
     </div>
   );
 };
