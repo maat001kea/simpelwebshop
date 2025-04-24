1a39c030a2b49a884a0c5c61bf1a88cf4f163805
Mahmoud Atie <maat0001@stud.kea.dk>
2025-04-24 08:42:18 +0200

    nyt

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -5,23 +5,29 @@ import { IoCartOutline } from "react-icons/io5";
 const Header = () => {
   return (
     <header className="grid grid-cols-subgrid" style={{ gridColumn: "full-bleed" }}>
-      <div className="grid grid-cols-[auto_1fr_auto] items-center py-4" style={{ gridColumn: "content" }}>
+      <div className="flex justify-between items-center py-4" style={{ gridColumn: "content" }}>
         {/* Left: Logo */}
-        <div className="flex items-center text-orange-500 text-4xl font-bold whitespace-nowrap">
-          <span>B</span>
-          <span className="text-orange-500 text-x4 font-semibold">uzz</span>
-          <span className="flex items-center text-orange-500 text-4xl font-bold whitespace-nowrap">B</span>
-          <span className="text-orange-500 text-x4 font-semibold">asket</span>
-        </div>
+        <div className="flex gap-10 items-baseline">
+          <div className="flex items-center text-orange-500 text-4xl font-bold whitespace-nowrap">
+            <Link className="flex items-center" href="/">
+              <span>B</span>
+              <span className="text-orange-500 text-x4 font-semibold">uzz</span>
+              <span className="flex items-center text-orange-500 text-4xl font-bold whitespace-nowrap">B</span>
+              <span className="text-orange-500 text-x4 font-semibold">asket</span>
+            </Link>
+          </div>
 
-        {/* Center: Product */}
-        <div className="flex justify-center">
-          <span className="text-3xl font-bold text-gray-900">Product</span>
+          {/* Center: Product */}
+          <div className="flex justify-center">
+            <Link href="/products">
+              <span className="text-3xl font-bold text-gray-900">Product</span>
+            </Link>
+          </div>
         </div>
 
         {/* Right: Cart */}
         <div className="flex justify-end text-orange-500 text-3xl ">
-          <Link href="/cart">
+          <Link href="/payment">
             <IoCartOutline className="hover:scale-110 transition-transform duration-200 cursor-pointer text-5xl" />
           </Link>
         </div>
