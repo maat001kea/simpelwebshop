9199310f66544ebfd1e746ef5925e504ec7db150
Farwa Rizvi <farwamehdi3@gmail.com>
2025-04-25 10:22:55 +0200

    add

--- src/app/components/CardBox.jsx
+++ src/app/components/CardBox.jsx
@@ -2,8 +2,8 @@
 import SingleButton from "./SingleViewButton";
 const CardBox = () => {
   return (
-    <div className="mt-32">
-      <div className="border-2 border-gray-300 rounded-lg p-4 mb-4">
+    <div className="mt-32  md:max-w-lg lg:max-w-xl border-2 border-gray-300 rounded-lg p-6 mb-4 space-y-4">
+      <div>
         <ul className="color-grey">buying product 1</ul>
         <ul className="color-grey">buying product 2</ul>
         <div className="mt-4 ">
