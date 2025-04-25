1ec1f486e2e4cbee76eba7c4fc7fb5f280e03a02
Mahmoud Atie <maat0001@stud.kea.dk>
2025-04-25 11:25:18 +0200

    nyt

--- src/app/components/SingleText.jsx
+++ src/app/components/SingleText.jsx
@@ -1,18 +1,22 @@
 "use client";
 import SingleButton from "./SingleViewButton";
-const SingleText = ({ title, brand, category, description, price }) => {
+
+const SingleText = (product) => {
+  const { title, brand, category, description, price } = product;
+
   return (
     <div className="mt-25 md:ml-0 md:mr-auto p-8 max-w-6xl mx-auto">
       <div className="flex flex-col gap-4">
         <div className="md:w-1/3 ml-0">
           <div className="p-6">
-            <h1 className="text-3xl text-gray-800 font-bold mb-4"> {title}</h1>
+            <h1 className="text-3xl text-gray-800 font-bold mb-4">{title}</h1>
             <h2 className="text-gray-700 mb-4">{brand}</h2>
             <p className="text-gray-500 mb-4">{category}</p>
             <p className="text-gray-700 mb-4">{description}</p>
-            <h3 className="text-gray-800  mb-4">{price}</h3>
+            <h3 className="text-gray-800 mb-4">{price} kr</h3>
             <div className="mt-4 p-4">
-              <SingleButton label="Tilføj til kurven" />
+              {/* 👇 Her sender du hele produktet videre til knappen */}
+              <SingleButton product={product} />
             </div>
           </div>
         </div>
