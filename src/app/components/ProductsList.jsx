c31cad6b1cace366ddf5e20d7bf63e5562029e19
Mahmoud Atie <maat0001@stud.kea.dk>
2025-04-28 09:29:47 +0200

    nyt

--- src/app/components/ProductsList.jsx
+++ src/app/components/ProductsList.jsx
@@ -7,7 +7,7 @@ const ProductsList = ({ data }) => {
     <div className="col-span-3">
       <ul className="grid grid-cols-3 gap-6 ">
         {data.map((item) => {
-          return <ProductCard key={item.id} id={item.id} title={item.title} rating={item.rating} price={item.price} discount={item.discountPercentage} img={item.images[1]} />;
+          return <ProductCard key={item.id} id={item.id} title={item.title} rating={item.rating} price={item.price} discount={item.discountPercentage} img={item.images[0]} />;
         })}
       </ul>
     </div>
